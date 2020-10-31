336c18ee3907398b900c3b6145b0c212aa1f4433
Brian Archibald <brianallenarchibald@gmail.com>
2020-10-31 17:03:37 -0500

    Refactor: Added writing to csv

--- speedtest.py
+++ speedtest.py
@@ -1,13 +1,24 @@
 import speedtest
 import datetime
+import csv
 import time
 
 s = speedtest.Speedtest()
+
+with open('test.csv' mode='w') as speedcsv:
+    csv_writer = csv.DictWriter(speedcsv, fieldnames=['time', 'downspeed', 'upspeed'])
+    csv_writer.writeheader()
 
 while True:
     time_now = datetime.datetime.now().strftime("%H:%M:%S")
     downspeed = round((round(s.download()) / 1048576), 2)
     upspeed = round((round(s.upload()) / 1048676), 2)
-    print('time :', time_now, 'downspeed :', downspeed, 'up speed :', upspeed)
+    csv_writer.writerow({
+        'time :', time_now,
+        'downspeed :', downspeed,
+        'up speed :', upspeed
+        })
+
+    # print('time :', time_now, 'downspeed :', downspeed, 'up speed :', upspeed)
 
     time.sleep(60)
